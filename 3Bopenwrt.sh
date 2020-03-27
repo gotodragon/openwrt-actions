bc09e09d0b6147d327b8989ee53feadfdfa95fda
Dragon <33269567+gotodragon@users.noreply.github.com>
2020-03-27 15:45:57 +0800

    Create 3Bopenwrt.sh

---
+++ 3Bopenwrt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: Allcloud
+# http://www.all-cloud.cn
+#=================================================
+#1. Modify default
+sed -i 's/192.168.1.1/10.0.0.111/g' openwrt/package/base-files/files/bin/config_generate
