4d2571ffb17a5dfc3fb108e1dcc986ecfec4e86e
Dragon <33269567+gotodragon@users.noreply.github.com>
2020-03-27 14:45:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,5 +6,5 @@
 # Author: Allcloud
 # http://www.all-cloud.cn
 #=================================================
-#1. Modify default IP 10.0.0.111可修改为你的IP地址
+#1. Modify default
 sed -i 's/192.168.1.1/10.0.0.111/g' openwrt/package/base-files/files/bin/config_generate
